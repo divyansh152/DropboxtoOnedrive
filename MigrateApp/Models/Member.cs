5b3b8fce173029e3ddbf0dc7b8fef219a30f318e
Divyansh Sharma <divyansh@nealanalytics.com>
2019-07-22 16:06:02 +0530

    Add project files.

---
+++ MigrateApp/Models/Member.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrateApp.Models
+{
+
+    public class MembersCollection
+    {
+        public Member[] members { get; set; }
+        public string cursor { get; set; }
+        public bool has_more { get; set; }
+    }
+
+    public class Member
+    {
+        public Profile profile { get; set; }
+        public Role role { get; set; }
+    }
+
+    public class Profile
+    {
+        public string team_member_id { get; set; }
+        public string account_id { get; set; }
+        public string email { get; set; }
+        public bool email_verified { get; set; }
+        public object[] secondary_emails { get; set; }
+        public Status status { get; set; }
+        public Name name { get; set; }
+        public Membership_Type membership_type { get; set; }
+        public DateTime joined_on { get; set; }
+        public string[] groups { get; set; }
+        public string member_folder_id { get; set; }
+        public string profile_photo_url { get; set; }
+        public DateTime suspended_on { get; set; }
+        public DateTime invited_on { get; set; }
+    }
+
+    public class Status
+    {
+        public string tag { get; set; }
+    }
+
+    public class Name
+    {
+        public string given_name { get; set; }
+        public string surname { get; set; }
+        public string familiar_name { get; set; }
+        public string display_name { get; set; }
+        public string abbreviated_name { get; set; }
+    }
+
+    public class Membership_Type
+    {
+        public string tag { get; set; }
+    }
+
+    public class Role
+    {
+        public string tag { get; set; }
+    }
+    public class FolderDetails
+    {
+        public Entities[] Entity { get; set; }
+
+    }
+    public class Entities
+    {
+        public string 
+    }
+}
---
+++ MigrateApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dropbox.Api;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace MigrateApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task t = new Task(GetAsync);
+            t.Start();
+            Console.WriteLine("Connecting to Dropbox...");
+            Console.ReadLine();
+
+        }
+        static async void GetAsync()
+        {
+           
+            string Baseurl = "https://api.dropboxapi.com/2/team/members/list";
+            var connectionurl = new Uri(@Baseurl);
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = connectionurl;
+             
+                HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Post, connectionurl);
+                req.Headers.Add("Authorization", "Bearer qdOwzAg1H0YAAAAAABBh5AJKwTCsoYK5shEDoBF1kJtGJv8Q7fXOBK2mYMFbnjZN");
+                HttpResponseMessage httpResponseMessage = await client.SendAsync(req);
+                httpResponseMessage.EnsureSuccessStatusCode();
+                HttpContent httpContent = httpResponseMessage.Content;
+                string responseString = await httpContent.ReadAsStringAsync();
+
+                Models.MembersCollection members = JsonConvert.DeserializeObject<Models.MembersCollection>(responseString);
+                foreach (var member in members.members)
+                {
+                    member.profile.team_member_id
+                }
+
+            }
+        }
+    }
+}
